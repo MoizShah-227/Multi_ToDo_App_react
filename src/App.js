f8465bf742b793682e4629e6c9cd3c0e20bc1ee7
MoizShah-227 <103181576+MoizShah-227@users.noreply.github.com>
2023-07-20 10:33:11 -0700

    Kind of Done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import Header from './components/Header';
 import Todoform from './components/Todoform';
+
 function App() {
   return (
    <>
    <Header/>
    <Todoform/>
-   
    </>
   );
 }
--- src/components/Todoform.js
+++ src/components/Todoform.js
@@ -2,16 +2,21 @@ import React, { useState } from 'react'
 
 const Todoform = () => {
     let    titleNo =0;
+    const array=[];
     const [inputtext, setInputtext] = useState('');
-    const [print,setPrint] =useState(false);    
-    const showData = (e) => {
-         e.preventDefault();        
-        console.log("Value: "+inputtext);
-        // <h1>value is:{inputtext}</h1>
-        
-    };
-
+    const [formData, setFormData] = useState({
+       title: '',
+        tasks:[
+            {
+                task:'',
+                id: new Date(),
+                isCompleted:false
+            }
+        ]
+    });
     
+
+
     //MULTIINPUT CODE
     const [data,setData]=useState([{fname:"",lname:""}])
    
@@ -29,20 +34,26 @@ const Todoform = () => {
         deleteVal.splice(i,1)
         setData(deleteVal)
     }
-    
+  
+ 
   return (
       <div className='container '>
-        <h1>{inputtext}</h1>
-        <h4>Title list</h4>
-        <form  className="TodoForm">
-            <input type="text" onSubmit={showData} value={inputtext} onChange={(e) => setInputtext(e.target.value)}className="todo-input" placeholder='Enter Task Title' />
         
-         </form>
-     
+        <h4>Title list</h4>
+        <form  className="TodoForm" onSubmit={e=>{
+            e.preventDefault();
+            setFormData({
+                ...formData,
+                title: inputtext,
+                titleNo: titleNo+1
+            })
+        }}>
+            <input type="text" value={inputtext} onChange={(e) => setInputtext(e.target.value)}  className="todo-input" placeholder='Enter Task Title' />        
+
      <h4 className='mt-2'>Task List </h4>
-     <div className="App">
+         <div className="App">
             {
-                data.map((val,i)=>
+                formData?.tasks?.map((val,i)=>
                 <div>
                     <input placeholder='Enter task' value={val.fname} onChange={(e)=>handleChange(e,i)} />
                     <button onClick={handleClick} className='btn btn-primary'>+</button><button className='btn-primary btn' onClick={()=>handleDelete(i)}>-</button>
@@ -50,20 +61,20 @@ const Todoform = () => {
                 )
             }
         </div>
-        {/* <button type="submit" onClick={()=>setPrint(true)} className='mt-3 btn btn-primary add-btn'>ADD</button> */}
 
-        <button type="submit" onClick={(e)=>showData(e)} className='mt-3 btn btn-primary add-btn'>ADD</button>
-            <div className='showContent pt-3'>
-            {
-                print?
-                <h3>{titleNo}:{inputtext}</h3>:
-                null
-                
-            }
-            </div>
-            
+        <button type="submit" className='mt-3 btn btn-primary 
+        add-btn'>ADD</button>
+         </form>
+     
+
+        
+        <div className='list'>
+        <h4>{formData?.titleNo}{formData?.title}</h4>
+        </div>
+    
      </div>       
 
+    
 );
 }
 
