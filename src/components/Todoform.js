c98f12aa6ba9a8d054c2d8df124a761a99bfd4c5
MoizShah-227 <103181576+MoizShah-227@users.noreply.github.com>
2023-07-20 12:45:23 -0700

    Not Done Yet!

--- src/components/Todoform.js
+++ src/components/Todoform.js
@@ -2,27 +2,33 @@ import React, { useState } from 'react'
 
 const Todoform = () => {
     let    titleNo =0;
-    const array=[];
+    let number=1;
     const [inputtext, setInputtext] = useState('');
     const [formData, setFormData] = useState({
-       title: '',
-        tasks:[
-            {
-                task:'',
-                id: new Date(),
-                isCompleted:false
+        title: '',
+         tasks:[
+             {
+                 task:'',
+                 id: new Date(),
+                 isCompleted:false,
+            
+                 list:'',
+                    id :new Date(),
+                    isCompleted: false,  
             }
-        ]
-    });
+        
+         ]
+     });
+       
     
-
-
     //MULTIINPUT CODE
-    const [data,setData]=useState([{fname:"",lname:""}])
-   
+    const [data,setData]=useState([{}]);
+    const [tasklist, setTasklist] =useState([]);
+
     const handleClick=()=>{
-        setData([...data,{fname:"",lname:""}])
+        setData([...data,{}]);
     }
+    
     const handleChange=(e,i)=>{
         const {name,value}=e.target
         const onchangeVal = [...data]
@@ -34,47 +40,50 @@ const Todoform = () => {
         deleteVal.splice(i,1)
         setData(deleteVal)
     }
-  
- 
+    
+    console.log(tasklist);
   return (
-      <div className='container '>
-        
+      <div className='container ' onSubmit={e=>{
+        e.preventDefault();
+        setFormData({
+            ...formData,
+            title: inputtext,
+            titleNo: titleNo+1,
+            list:tasklist,
+            number:number+1
+                    
+        })
+    }}>
         <h4>Title list</h4>
-        <form  className="TodoForm" onSubmit={e=>{
-            e.preventDefault();
-            setFormData({
-                ...formData,
-                title: inputtext,
-                titleNo: titleNo+1
-            })
-        }}>
-            <input type="text" value={inputtext} onChange={(e) => setInputtext(e.target.value)}  className="todo-input" placeholder='Enter Task Title' />        
-
+        <form  className="TodoForm">
+            <input type="text"  value={inputtext} onChange={(e) => setInputtext(e.target.value)}className="todo-input" placeholder='Enter Task Title' />
+            <button type="submit" className='mt-3 btn btn-primary add-btn'>ADD</button>
+    
+         </form>
+     
      <h4 className='mt-2'>Task List </h4>
-         <div className="App">
+     <div className="App">
             {
-                formData?.tasks?.map((val,i)=>
+                <form>{
+                data.map((val,i)=>
                 <div>
-                    <input placeholder='Enter task' value={val.fname} onChange={(e)=>handleChange(e,i)} />
+                    <input placeholder='Enter task'  value={tasklist}  onChange={(e)=> setTasklist(e.target.value)}/>
                     <button onClick={handleClick} className='btn btn-primary'>+</button><button className='btn-primary btn' onClick={()=>handleDelete(i)}>-</button>
                 </div>
                 )
+        
+            }
+                </form>
             }
         </div>
-
-        <button type="submit" className='mt-3 btn btn-primary 
-        add-btn'>ADD</button>
-         </form>
-     
+       
 
         
         <div className='list'>
         <h4>{formData?.titleNo}{formData?.title}</h4>
-        </div>
-    
-        
+        <h3>{formData?.number}{formData?.list}</h3>
+        </div>             
      </div>       
-
     
 );
 }
